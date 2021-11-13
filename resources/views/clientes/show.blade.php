b52fafe6688a459738fdf9561356b3d4faec80e3
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2021-11-12 22:20:18 -0400

    22:20

--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -12,7 +12,7 @@
     <div class="card-body">
       <form class="needs-validation" novalidate>
 
-        <div class="form-row">
+        {{-- <div class="form-row">
 
           <div class="col-md-4 mb-3">
             <label for="validationCustom01">Cliente</label>
@@ -78,10 +78,72 @@
             </div>
           </div>
 
-        </div>
+        </div> --}}
 
-        <div class="form-row">
+        <div class="container-first">
+          <div class="row">
+            <div class="col-md-9">
+              <div class="col-md-4 mb-3">
+                <label for="validationCustom01">Cliente</label>
+                <input type="text" class="form-control" id="validationCustom01" value="" required>
+              </div>
+              <div class="col-md-4 mb-3">
+                <label for="validationCustom01">Tipo Cliente</label>
+                <input type="text" class="form-control" id="validationCustom01" value="" required>
+              </div>
+              <div class="col-md-4 mb-3">
+                <label for="validationCustom01">Credito Habilitado</label>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="creditradio" id="creditradio1">
+                  <label class="form-check-label" for="flexRadioDefault1">
+                    Si
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="creditradio" id="creditradio2">
+                  <label class="form-check-label" for="flexRadioDefault2">
+                    No
+                  </label>
+                </div>
+              </div>
+              <div class="col-md-4 mb-3">
+                <label for="validationCustom01">Habilitado para Compras</label>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="buyradio" id="buyradio1">
+                  <label class="form-check-label" for="flexRadioDefault1">
+                    Si
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="buyreadio" id="buyradiot2">
+                  <label class="form-check-label" for="flexRadioDefault2">
+                    No
+                  </label>
+                </div>
+              </div>
+              <div class="col-md-4 mb-3">
+                <label for="validationCustom01">Habilitado para Descuentos</label>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="descradio" id="descradio1">
+                  <label class="form-check-label" for="flexRadioDefault1">
+                    Si
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="descradio" id="descradio2">
+                  <label class="form-check-label" for="flexRadioDefault2">
+                    No
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">2
+            </div>
+          </div>
 
+          <div clas="row">
+
+          </div>
         </div>
 
 
--- resources/views/reportes/index.blade.php
+++ resources/views/reportes/index.blade.php
@@ -1,67 +1,120 @@
 @extends('layouts.app')
 @section('content')
 <style type="text/css">
-		html,body{
-			overflow: hidden;
-			background: white;
-		}
-	</style>
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-	<style type="text/css">
+  html,
+  body {
+    overflow: hidden;
+    background: white;
+  }
 </style>
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="">Clientes</a></li>
-  </ol>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<style type="text/css">
+</style>
+<ol class="breadcrumb">
+  <li class="breadcrumb-item"><a href="">Clientes</a></li>
+</ol>
 <div class="container-fluid">
   <div class="card">
     <div class="card-header">
       <h2>Listado de Clientes</h2><br />
     </div>
-    <div class="card-body">
+    <div class="card-body row">
 
-      <form>
-        <div class="form-row">
+      <form class="form-row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-4 mb-3 ">
+              <label for="ccmonth">Servicios</label>
+              <select class="form-control" id="ccmonth">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+              </select>
+            </div>
 
-          <div class="col-md-4 mb-3">
-            <label for="ccmonth">Servicios</label>
-            <select class="form-control" id="ccmonth">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-            </select>
-          </div>
+            <div class="col-md-4 mb-3 ">
+                <label for="ccmonth">Fecha Desde</label>
+                <select class="form-control" id="ccmonth">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                </select>
+            </div>
 
-          <div class="col-md-4 mb-3">
-            <label for="ccmonth">Fecha Desde</label>
-            <select class="form-control" id="ccmonth">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-            </select>
-          </div>
-
-          <div class="col-md-4 mb-3">
-            <label for="ccmonth">Fecha Hasta</label>
-            <select class="form-control" id="ccmonth">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-            </select>
-          </div>
-
-        </div>
-
-
-        <div class="form-row">
-
-          <div class="form-row ">
-            <div style="width: 600px; height: 100px;">
-              <canvas id="chartOne" width="100" height="100"></canvas>
+            <div class="col-md-4 mb-3">
+              <label for="ccmonth">Fecha Hasta</label>
+              <select class="form-control" id="ccmonth">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+              </select>
             </div>
           </div>
-
         </div>
+
+        <div class="col-md-12">
+            <div style="width: 400px; height: 100px;">
+              <canvas id="chartOne" width="100" height="100"></canvas>
+            </div>
+        </div>
+        <hr>
+
+
+        <div class="col-md-12">
+          <div class="col-md-4">
+          <table style="width: 100%">
+            <tr>
+              <td style="width:100%; padding: 0 20px;">
+                <div class="form-group">
+                  <div class="input-group">
+                    <label for="ccmonth">Tipo Cliente</label>
+                    <select class="form-control" id="ccmonth">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                    </select>
+                  </div>
+                </td>
+              </td>
+            </tr>
+
+            <tr>
+              <td style="width:100%; padding: 0 20px;">
+                <div class="form-group">
+                  <div class="input-group">
+                    <label for="ccmonth">Fecha Desde</label>
+                    <select class="form-control" id="ccmonth">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                    </select>
+                  </div>
+                </td>
+              </td>
+            </tr>
+
+            <tr>
+              <td style="width:100%; padding: 0 20px;">
+                <div class="form-group">
+                  <div class="input-group">
+                    <label for="ccmonth">Fecha Hasta</label>
+                    <select class="form-control" id="ccmonth">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                    </select>
+                  </div>
+                </td>
+              </td>
+            </tr>
+
+          </table>
+        </div>
+
+
+
+
 
       </form>
 
@@ -73,34 +126,31 @@
 
 
 <script type="text/javascript">
-		var ctxOne = document.getElementById("chartOne");
-		var chartOne = new Chart(ctxOne, {
-			type: 'bar',
-			data: {
-				labels: ["2011 - Ene","2011 - Feb","2011 - Mar","2011 - Abr","2011 - May",
-                "2011 - Jun","2011 - Jul","2011 - Ago","2011 - Sep","2011 - Oct",
-                "2011 - Nov","2011 - Dic", "2012 - Ene","2012 - Feb","2012 - Mar",
-                "2012 - Abr","2012 - May","2012 - Jun"],
-				datasets: [
-				{
-					label: "Record de Acción",
-					backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#a65c6d","#24c412","#bb1112"],
-					data: [1,2,4,5,2,4,1,3,1,3,2,1,4,2,4,2,1,2]
-				}
-				]
-			},
-			options: {
-				cutoutPercentage: 70,
-				responsive: true,
-				maintainAspectRatio: true,
-				scales: {
-					yAxes: [{
-						ticks: {
-							beginAtZero: true
-						}
-					}]
-				}
-			}
-		});
-	</script>
+  var ctxOne = document.getElementById("chartOne");
+  var chartOne = new Chart(ctxOne, {
+    type: 'bar',
+    data: {
+      labels: ["2011 - Ene", "2011 - Feb", "2011 - Mar","2011 - Ene", "2011 - Feb", "2011 - Mar","2011 - Ene", "2011 - Feb", "2011 - Mar","2011 - Ene", "2011 - Feb", "2011 - Mar"],
+      datasets: [
+        {
+          label: "Record de Acción",
+          backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
+          data: [1, 2, 3,1,2,3,1,2,3,1,2,3]
+        }
+      ]
+    },
+    options: {
+      cutoutPercentage: 70,
+      responsive: true,
+      maintainAspectRatio: true,
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+</script>
 @endsection

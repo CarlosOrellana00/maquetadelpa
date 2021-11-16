bb0b17ec1d2b9b0a9a537b5c531ef63be5c69240
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2021-11-16 16:10:00 -0400

    16-11 16-09

--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -9,13 +9,115 @@
     <div class="card-header">
       <h2>Ficha Cliente</h2><br />
     </div>
+
     <div class="card-body">
       <form class="needs-validation" novalidate>
         <div class="container-first">
 
           <div class="row">
+            <div class="col-9">
+
+              <div class="row">
+
+                <div class="col-6 mb-3">
+                  <label for="validationCustom01">Cliente</label>
+                  <input type="text" class="form-control" id="validationCustom01" value="" required>
+                </div>
+
+                <div class="col-6">
+                   <label for="validationCustom01">Tipo Cliente</label>
+                  <input type="text" class="form-control" id="validationCustom01" value="" required>
+                </div>
+
+              </div>
+
+              <div class="row">
+
+                <div class="col-4">
+                  <label for="validationCustom01">Credito Habilitado</label>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="creditradio" id="creditradio1">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                      Si
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="creditradio" id="creditradio2">
+                    <label class="form-check-label" for="flexRadioDefault2">
+                      No
+                    </label>
+                  </div>
+                </div>
+
+                <div class="col-4">
+                  <label for="validationCustom01">Habilitado para Compras</label>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="buyradio" id="buyradio1">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                      Si
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="buyreadio" id="buyradiot2">
+                    <label class="form-check-label" for="flexRadioDefault2">
+                      No
+                    </label>
+                  </div>
+                </div>
+
+                <div class="col-4">
+                  <label for="validationCustom01">Habilitado para Descuentos</label>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="descradio" id="descradio1">
+                      <label class="form-check-label" for="flexRadioDefault1">
+                        Si
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="descradio" id="descradio2">
+                      <label class="form-check-label" for="flexRadioDefault2">
+                        No
+                      </label>
+                    </div>
+                </div>
+
+              </div>
+            </div>
+
+            <div class="col-3">
+              <div class="card">
+                <a class="btn btn-primary mb-3" href="" role="button">Descuentos</a>
+                <a class="btn btn-primary mb-3" href="" role="button">Empresas</a>
+                <a class="btn btn-primary mb-3" href="" role="button">Cotizaciones</a>
+                <a class="btn btn-primary mb-3" href="" role="button">Envios</a>
+                <a class="btn btn-primary mb-3" href="" role="button">Pagos</a>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="row">
+
+            <div class="col-3">
+              casillas
+              <div class="row">
+                web
+              </div>
+              <div class="row">
+                chat
+              </div>
+            </div>
+
+            <div class="col-9">
+              imagen/grafico
+            </div>
+
+          </div>
+
+
+          {{-- <div class="row">
             <div class="col-md-7">
-              {{-- primera fila --}}
+
               <div class="row">
                 <div class="col-4">
                   <label for="validationCustom01">Cliente</label>
@@ -41,7 +143,7 @@
                   </div>
                 </div>
               </div>
-              {{-- 2da fila --}}
+
               <div class="row">
                 <div class="col-6">
                   <label for="validationCustom01">Habilitado para Compras</label>
@@ -61,18 +163,18 @@
 
                 <div class="col-6">
                   <label for="validationCustom01">Habilitado para Descuentos</label>
-                  <div class="form-check">
-                    <input class="form-check-input" type="radio" name="descradio" id="descradio1">
-                    <label class="form-check-label" for="flexRadioDefault1">
-                      Si
-                    </label>
-                  </div>
-                  <div class="form-check">
-                    <input class="form-check-input" type="radio" name="descradio" id="descradio2">
-                    <label class="form-check-label" for="flexRadioDefault2">
-                      No
-                    </label>
-                  </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="descradio" id="descradio1">
+                      <label class="form-check-label" for="flexRadioDefault1">
+                        Si
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="descradio" id="descradio2">
+                      <label class="form-check-label" for="flexRadioDefault2">
+                        No
+                      </label>
+                    </div>
                   </div>
                 </div>
 
@@ -88,7 +190,7 @@
                 <a class="btn btn-primary mb-3" href="" role="button">Pagos</a>
               </div>
             </div>
-          </div>
+          </div> --}}
           {{-- SEGUNDA BANDA --}}
         </div>
 
